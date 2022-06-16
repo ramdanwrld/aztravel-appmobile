7be3d781767b54401e850233c972c018299932e6
ramdanwrld <ramdanwrld@gmail.com>
2022-06-16 16:11:49 +0700

    update login

--- lib/login_register/login.dart
+++ lib/login_register/login.dart
@@ -3,8 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/login_register/signup.dart';
 import 'package:flutter_application_1/bottom/beranda.dart';
 import 'package:flutter_application_1/bottom/bottom_navi.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 import '../bottom/bottom_navi.dart';
+import 'package:http/http.dart' as http;
 
 class login extends StatefulWidget {
   const login({Key? key}) : super(key: key);
@@ -64,6 +67,7 @@ class _loginState extends State<login> {
               onPressed: () {
                 // Navigator.push(context,
                 //     MaterialPageRoute(builder: (context) => BottomNavi()));
+                this._doLogin();
               },
             ),
             TextButton(
@@ -80,5 +84,31 @@ class _loginState extends State<login> {
         ),
       ),
     );
+  }
+
+  Future _doLogin() async {
+    if (txtEmail.text.isEmpty || txtPassword.text.isEmpty) {
+      Alert(context: context, title: "Data Tidak Benar", type: AlertType.error)
+          .show();
+      return;
+    }
+    ProgressDialog progressDialog = ProgressDialog(context);
+    progressDialog.style(message: "Tunggu...");
+    progressDialog.show();
+    var url = 'http://127.0.0.1:8000/api/login';
+    final response = await http.post(Uri.parse(url), body: {
+      'Email': txtEmail.text,
+      'Password': txtPassword.text,
+    }, headers: {
+      'Accept': 'application/json'
+    });
+    progressDialog.hide();
+    if (response.statusCode == 200) {
+      Alert(context: context, title: "Login Berhasil", type: AlertType.success)
+          .show();
+    } else {
+      Alert(context: context, title: "Login Gagal", type: AlertType.error)
+          .show();
+    }
   }
 }
